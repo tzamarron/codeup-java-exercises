3b3b603be9260a62aca4e2b274416ff829a05d74
Ted Zamarron <ted.zamarron@gmail.com>
2020-03-29 13:57:39 -0500

    added Rectangle and Square classes

---
+++ src/shapes/Rectangle.java
@@ -0,0 +1,21 @@
+package shapes;
+
+public class Rectangle {
+    protected long length;
+    protected long width;
+
+    public Rectangle(long newLength, long newWidth){
+        this.length = newLength;
+        this.width = newWidth;
+    }
+
+    public long getArea(){
+        return this.length * this.width;
+    }
+
+    public long getPerimeter(){
+        return (this.length*2) + (this.width*2);
+    }
+
+
+}
---
+++ src/shapes/Square.java
@@ -0,0 +1,11 @@
+package shapes;
+
+public class Square extends Rectangle{
+    protected double length;
+    protected double width;
+
+
+    public Square(long side) {
+        super(newLength, newWidth);
+    }
+}
