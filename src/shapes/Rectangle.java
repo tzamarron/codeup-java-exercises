5fee3b8185e1684e91841d4fb22958b404bdc75c
Ted Zamarron <ted.zamarron@gmail.com>
2020-03-31 16:00:33 -0500

    modifications

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -2,22 +2,6 @@ package shapes;
 
 public class Rectangle extends Quadrilateral implements Measurable {
 
-//    protected long length;
-//    protected long width;
-//
-//    public Rectangle(long newLength, long newWidth) {
-//        this.length = newLength;
-//        this.width = newWidth;
-//    }
-//
-//    public long getArea() {
-//        return length * width;
-//    }
-//
-//    public long getPerimeter() {
-//        return (length * 2) + (width * 2);
-//    }
-
     public Rectangle(long newLength, long newWidth) {
         super(newLength, newWidth);
     }
@@ -41,4 +25,20 @@ public class Rectangle extends Quadrilateral implements Measurable {
     public long getArea() {
         return length * width;
     }
+
+    //    protected long length;
+//    protected long width;
+//
+//    public Rectangle(long newLength, long newWidth) {
+//        this.length = newLength;
+//        this.width = newWidth;
+//    }
+//
+//    public long getArea() {
+//        return length * width;
+//    }
+//
+//    public long getPerimeter() {
+//        return (length * 2) + (width * 2);
+//    }
 }
