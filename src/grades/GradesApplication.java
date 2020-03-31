642431943dc2d691d559bb1523070c1645aba4a4
Ted Zamarron <ted.zamarron@gmail.com>
2020-03-31 18:57:55 -0500

    completed exercise 5

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -16,21 +16,33 @@ public class GradesApplication {
         deathKnight.addGrade(100);
         deathKnight.addGrade(100);
         deathKnight.addGrade(85);
+        deathKnight.recordAttendance("2020-03-21","present");
+        deathKnight.recordAttendance("2020-03-22","absent");
+        deathKnight.recordAttendance("2020-03-23","present");
 
         Student mage = new Student("Braindmg");
         mage.addGrade(100);
         mage.addGrade(100);
         mage.addGrade(95);
+        mage.recordAttendance("2020-03-21","present");
+        mage.recordAttendance("2020-03-22","present");
+        mage.recordAttendance("2020-03-23","present");
 
         Student warlock = new Student("Panic");
         warlock.addGrade(100);
         warlock.addGrade(100);
         warlock.addGrade(90);
+        warlock.recordAttendance("2020-03-21","absent");
+        warlock.recordAttendance("2020-03-22","present");
+        warlock.recordAttendance("2020-03-23","present");
 
         Student hunter = new Student("Nyx");
         hunter.addGrade(100);
         hunter.addGrade(100);
         hunter.addGrade(85);
+        hunter.recordAttendance("2020-03-21","absent");
+        hunter.recordAttendance("2020-03-22","absent");
+        hunter.recordAttendance("2020-03-23","present");
 
         // Place username and Student associated to it in hashset students
         students.put("crodaed4LichKing", deathKnight);
@@ -54,10 +66,11 @@ public class GradesApplication {
             System.out.println("\t0 - View Realm Average");
             System.out.println("\t1 - View past event points for all guilds");
             System.out.println("\t2 - View info for all guilds");
-            for(int i = 0; i < l.size(); i++){
-                System.out.printf("\t%d - View info for %s guild\n",counter,l.get(i));
+            for (String s : l) {
+                System.out.printf("\t%d - View info for %s guild\n", counter, s);
                 counter++;
-            };
+            }
+            ;
 
             // Asks user what guild to get info on and records response
             int userInquires = test.getInt(0,counter-1,"What would you like to see?\n> ");
@@ -98,8 +111,10 @@ public class GradesApplication {
         String guild = list.get(usableNum);
         System.out.printf("\tGuild : %s\n" +
                 "\tGuild Leader : %s\n" +
+                "\tGuild raid days missed : %s\n" +
+                "\tGuild Attendance Avg : %.2f\n" +
                 "\tGuild past event points : %s\n"  +
-                "\tGuild Average : %.2f\n\n",guild,hashmap.get(guild).getName(),hashmap.get(guild).getGrades(),hashmap.get(guild).getGradeAverage());
+                "\tGuild Average : %.2f\n\n",guild,hashmap.get(guild).getName(),hashmap.get(guild).daysAbsent(),hashmap.get(guild).attendancePercent(),hashmap.get(guild).getGrades(),hashmap.get(guild).getGradeAverage());
     }
 
     public static void overallAvg(HashMap<String, Student> hashmap, List<String> list){
@@ -111,16 +126,16 @@ public class GradesApplication {
     }
 
     public static void allPoints(HashMap<String, Student> hashmap, List<String> list){
-        for (int i = 0; i < list.size(); i++) {
-            System.out.printf("\tGuild : %s\n", list.get(i));
-            System.out.printf("\tGuild past event points : %s\n\n",hashmap.get(list.get(i)).getGrades());
+        for (String s : list) {
+            System.out.printf("\tGuild : %s\n", s);
+            System.out.printf("\tGuild past event points : %s\n\n", hashmap.get(s).getGrades());
         }
     }
 
     public static void csvReport(HashMap<String, Student> hashmap, List<String> list){
-        for (int i = 0; i < list.size(); i++){
-            String guild = list.get(i);
-            System.out.printf("\t%s, %s, %s, %.2f\n",guild,hashmap.get(guild).getName(),hashmap.get(guild).getGrades(),hashmap.get(guild).getGradeAverage());
+        System.out.println("\tGuild, Guild Master, Raid Days Missed, Attendance Avg, Event Points, Guild Average");
+        for (String guild : list) {
+            System.out.printf("\t%s, %s, %s, %.2f, %s, %.2f\n", guild, hashmap.get(guild).getName(), hashmap.get(guild).daysAbsent(), hashmap.get(guild).attendancePercent(), hashmap.get(guild).getGrades(), hashmap.get(guild).getGradeAverage());
         }
     }
 
